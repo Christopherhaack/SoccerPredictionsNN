6a34f639a38eef258daa342a427d03562ea3a09f
Christopherhaack <christophermoorehaack@gmail.com>
2017-02-16 12:28:49 -0800

    added some comments to describe what was going on
    comments include

--- data_import.py
+++ data_import.py
@@ -9,9 +9,17 @@ conn = sqlite3.connect('C:\\Users\ChristopherHaack\Downloads\soccer\database.sql
 c = conn.cursor()
 
 def getPlayerStats(player_ids, home, date ):
+    ''' this is a helper function that allows one to get satistical information loaded in the fifa
+    video game for a given player on a given date. This allows us to know how good a player is in fifa
+    @param player_ids a list of player id's that correspond to the fifa_api_ids of certain players
+    @param home, is the team were looking at the home team
+    @param date, date of the game
+    @ return a list of player stats.
+    '''
     s = []
     
     margin = datetime.timedelta(days = 365)
+    #always returns the teams players first for the data structure.
     if home == False:
         temp = player_ids[11:] + player_ids[:11]
         player_ids = temp
@@ -52,12 +60,13 @@ def getPlayerStats(player_ids, home, date ):
 
 def getShotInfo(sInfo, team_id):
     '''
-    helper function to count how many shots each team has
+    helper function to count how many shots each team has from an xml format
     '''
     cnt1 = 0
     cnt2 = 0
     shotTree = ET.fromstring(sInfo)
     for elem in shotTree:
+        #each shot is loaded as a set of elements and right now we only care about quantity
         for item in elem:
             if item.text == str(team_id):
                 cnt1 += 1
@@ -67,7 +76,7 @@ def getShotInfo(sInfo, team_id):
 
 def getPosInfo(pInfo, home):
     '''
-    helper function to see posession break down of the game
+    helper function to see posession break down of the game at halftime and at full time
     '''
     posTree = ET.fromstring(pInfo)
     vals = [0, 0 ,0 ,0]
@@ -98,10 +107,16 @@ def getPosInfo(pInfo, home):
 
     
 def gamesMapping(team_id, games, normalize=False):
+    ''' this is a helperfunction that returns performance information for different games
+    @param team_id is the id of the team
+    @param games is the matches it played in
+    @normalize attempts to normalize data set
+    @return a performance which is a set of lists with stats about each game.
+    '''
     performance = []
     for game in games:
         p = []
-        #adds 
+        #adds the stage of the season the game is in 
         p.append(game[4])
         #checks to see if the team is home team and adds result, which is given by [points, goals scored, goals against]
         if game[7] == team_id:
@@ -128,6 +143,7 @@ def gamesMapping(team_id, games, normalize=False):
             p.append(game[9])
         #these will give positions of players
         p += game[12:55]
+        #these will give stats of players
         p += getPlayerStats(game[55:77], home, game[5])
         #parses info about shots on target and gives (shots on target, shots against)
         p += getShotInfo(game[78], team_id)
@@ -142,6 +158,7 @@ def gamesMapping(team_id, games, normalize=False):
 
 
 class season:
+    ''' this is a class which will hold performances over a set of seasons'''
     def __init__(self, season = '2015/2016'):
         season = '2015/2016'
         selectS = ''' select league_id from match '''
@@ -155,6 +172,7 @@ class season:
         self.leagues = leagues
         self.season = season
 class leagueSeason:
+    ''' this class will hold the information of a given leagues season'''
     def __init__(self, league_id = 1729, season = '2015/2016'):
         statement = '''select home_team_api_id
     from match where'''
@@ -183,6 +201,8 @@ class leagueSeason:
             performances.append(p)
         return performances
     def getSeasonInfo(self):
+        ''' class that is used to determine y values, basically just gives
+        the two teams playing the game and the result and the stage of the season'''
         statement = '''select stage,
         home_team_api_id,
         away_team_api_id, 
